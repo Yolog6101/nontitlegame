cd0a0f8eccae6734596af5f7c7603ddc8f80c30a
Yuzuki Oe <oe-yuzuki-vs@ynu.jp>
2023-05-23 17:36:48 +0900

    ボタン作成

---
+++ Nontiltle_game/Assets/Scenes/SceneButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    public string scenename;
+
+    public void OnButton(){
+        SceneManager.LoadScene(scenename);
+    }
+    
+}
